93229a56eb25dbe5b44817f15b1b4c3ddbbf9ca2
Denis <denis.shagim@icloud.com>
2025-01-06 18:05:09 +0500

    feat: Added markup with map

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 import { validateIp } from "./helpers";
+import { addTileLayer } from "./helpers";
+import icon from "../images/icon-location.svg";
 
 const ipInput = document.querySelector(".search-bar__input");
 const btn = document.querySelector("button");
@@ -10,6 +14,20 @@ const ispInfo = document.querySelector("#isp");
 
 btn.addEventListener("click", getData);
 ipInput.addEventListener("keydown", handleKey);
+
+const markerIcon = L.icon({
+  iconUrl: icon,
+  iconSize: [30, 40],
+  //   iconAnchor: [22, 94],
+});
+
+const mapArea = document.querySelector(".map");
+const map = L.map(mapArea, {
+  center: [51.505, -0.09],
+  zoom: 13,
+});
+addTileLayer(map);
+L.marker([51.505, -0.09], { icon: markerIcon }).addTo(map);
 
 function getData() {
   if (validateIp(ipInput.value)) {
@@ -35,3 +53,7 @@ function setInfo(mapData) {
   timezoneInfo.innerText = mapData.location.timezone;
   ispInfo.innerText = mapData.isp;
 }
+
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     maxZoom: 19,
+//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' }).addTo(map);
